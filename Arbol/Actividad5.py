37c47ac287998023e72afe23d92fb3ecc2bf57e9
SebastianDiaz <149125121+Sebita20@users.noreply.github.com>
2024-11-05 17:22:26 -0300

    new file:   Arbol/Actividad5.py 	new file:   Arbol/Funciones.py 	new file:   Arbol/Funciones2.py

---
+++ Arbol/Actividad5.py
@@ -0,0 +1,209 @@
+from Funciones2 import BinaryTree
+
+super_heroes = [
+  {
+    "nombre": "Linterna Verde",
+    "año_aparicion": 1940,
+    'is_hero': True,
+    "casa_comic": "DC Comics",
+    "biografia": "Miembro de la Tropa de Linternas Verdes, posee un anillo que le otorga poderes basados en la fuerza de voluntad."
+  },
+  {
+    "nombre": "Wolverine",
+    "año_aparicion": 1974,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Mutante con garras retráctiles y habilidades regenerativas, miembro de los X-Men."
+  },
+  {
+    "nombre": "Dortor Strange",
+    "año_aparicion": 1963,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Hechicero supremo del universo Marvel, maestro de las artes místicas y protector de la realidad."
+  },
+  {
+    "nombre": "Capitana Marvel",
+    "año_aparicion": 1968,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Heroína cósmica con poderes de vuelo, fuerza sobrehumana y energía cósmica."
+  },
+  {
+    "nombre": "Mujer Maravilla",
+    "año_aparicion": 1941,
+    'is_hero': True,
+    "casa_comic": "DC Comics",
+    "biografia": "Princesa amazona y una de las principales defensoras de la justicia y la igualdad en el Universo DC."
+  },
+  {
+    "nombre": "Flash",
+    "año_aparicion": 1940,
+    'is_hero': True,
+    "casa_comic": "DC Comics",
+    "biografia": "Velocista con la capacidad de correr a velocidades superiores a la luz, miembro de la Liga de la Justicia."
+  },
+  {
+    "nombre": "Star-Lord",
+    "año_aparicion": 1976,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Líder de los Guardianes de la Galaxia, experto en combate y estrategia intergaláctica."
+  },
+  {
+    "nombre": "Superman",
+    "año_aparicion": 1938,
+    'is_hero': True,
+    "casa_comic": "DC Comics",
+    "biografia": "El Hombre de Acero, uno de los héroes más icónicos de DC con superpoderes sobrehumanos."
+  },
+  {
+    "nombre": "Batman",
+    "año_aparicion": 1939,
+    'is_hero': True,
+    "casa_comic": "DC Comics",
+    "biografia": "El Caballero Oscuro, detective y luchador experto que protege Gotham City."
+  },
+  {
+    "nombre": "Iron Man",
+    "año_aparicion": 1963,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Tony Stark, genio multimillonario y superhéroe con una armadura tecnológica de alta tecnología."
+  },
+  {
+    "nombre": "Wonder Woman",
+    "año_aparicion": 1941,
+    'is_hero': True,
+    "casa_comic": "DC Comics",
+    "biografia": "La princesa amazona Diana, guerrera y defensora de la paz y la justicia en el mundo."
+  },
+  {
+    "nombre": "Spider-Man",
+    "año_aparicion": 1962,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Peter Parker, joven héroe con habilidades arácnidas tras ser picado por una araña radiactiva."
+  },
+  {
+    "nombre": "Thor",
+    "año_aparicion": 1962,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Dios nórdico del trueno y miembro de los Vengadores, posee un martillo encantado llamado Mjolnir."
+  },
+  {
+    "nombre": "Aquaman",
+    "año_aparicion": 1941,
+    'is_hero': True,
+    "casa_comic": "DC Comics",
+    "biografia": "Rey de Atlantis con la capacidad de comunicarse con la vida marina y controlar el agua."
+  },
+  {
+    "nombre": "Green Arrow",
+    "año_aparicion": 1941,
+    'is_hero': True,
+    "casa_comic": "DC Comics",
+    "biografia": "Oliver Queen, arquero habilidoso y defensor de la justicia con su arco y flechas."
+  },
+  {
+    "nombre": "Hulk",
+    "año_aparicion": 1962,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Bruce Banner, científico transformado en monstruo verde con fuerza increíble."
+  },
+  {
+    "nombre": "Black Widow",
+    "año_aparicion": 1964,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Natasha Romanoff, espía rusa y experta en combate mano a mano y armas."
+  },
+  {
+    "nombre": "Mr. Fantástico",
+    "año_aparicion": 1961,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Líder de los 4 Fantásticos, científico brillante con la capacidad de estirar y deformar su cuerpo."
+  },
+  {
+    "nombre": "La Mujer Invisible",
+    "año_aparicion": 1961,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Miembro de los 4 Fantásticos, posee el poder de hacerse invisible y crear campos de fuerza."
+  },
+  {
+    "nombre": "La Alforcha Humana",
+    "año_aparicion": 1961,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Miembro de los 4 Fantásticos, puede envolverse en llamas y volar a altas velocidades."
+  },
+  {
+    "nombre": "La Cosa",
+    "año_aparicion": 1961,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "Miembro de los 4 Fantásticos, posee una fuerza y resistencia sobrehumanas, con piel rocosa."
+  },
+  {
+    "nombre": "Capitán América",
+    "año_aparicion": 1941,
+    'is_hero': True,
+    "casa_comic": "Marvel Comics",
+    "biografia": "El supersoldado Steve Rogers, símbolo de libertad y justicia con escudo indestructible."
+  },
+  {
+    "nombre": "Thanos",
+    'is_hero': False
+  },
+  {
+    "nombre": "Doctor Doom",
+    'is_hero': False
+  },
+  {
+    "nombre": "Green Gobling",
+    'is_hero': False
+  }
+]
+
+tree = BinaryTree
+heroes = BinaryTree
+villanos = BinaryTree
+
+for personajes in super_heroes: 
+    if personajes ['is_hero'] == False:
+        villanos.insert_node(personajes['nombre'], personajes)
+    else:
+        tree.insert_node(personajes['nombre'], personajes)
+        heroes.insert_node(personajes['nombre'], personajes)
+
+lista_villanos = villanos.inorden_villanos()
+print("villanos: ", lista_villanos)
+
+print("SuperHeroes que comienzan con C: ")
+heroes.inorden_superheros_start_with("C")
+
+print("----------------------")
+contar= tree.contar_super_heroes()
+print("Hay ", contar, "superheroes")
+
+print("----------------------")
+pos= tree.search('Doctor Strange')
+pos.value= 'Doctor Strange'
+
+print("----------------------")
+tree.postorden()
+
+print("----------------------")
+villanos.inorden()
+print("----------------------")
+heroes.inorden()
+
+print("----------------------")
+nodos_villanos = villanos.contar_nodos()
+nodos_heroes = heroes.contar_nodos()
+print("El árbol de villanos tiene", nodos_villanos, "nodos")
+print("El árbol de héroes tiene", nodos_heroes, "nodos")
